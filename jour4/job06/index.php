fad6f2c936a5e2756655d4264bf6b86c698c8028
NicolasParvedyLaPlateforme <nicolas.parvedy@laplateforme.io>
2025-07-26 22:24:34 +0200

    job07

--- jour4/job06/index.php
+++ jour4/job06/index.php
@@ -6,8 +6,26 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
+    <form action="" method="post" class="form-example">
+        <div class="form-example">
+            <label for="name">Enter your name: </label>
+            <input type="number" name="nombre" id="nombre" required />
+        </div>
+        <div class="form-example">
+            <input type="submit" value="pair or impair" />
+        </div>
+    </form>
 
-    ?>
+    
+    <?php
+        if (isset($_POST['nombre'])){
+            if ($_POST['nombre'] % 2 == 0){
+                echo "<p>Le nobmre est pair</p>";
+            }else {
+            echo "<p>Le nombre est impair</p>";
+            }
+        }
+
+        ?>
 </body>
 </html>
